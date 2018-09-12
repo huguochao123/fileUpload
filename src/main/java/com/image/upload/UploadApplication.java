42904aa8d82f58e6a74fb992eefa548826e34af8
unknown <huguochao@gzkit.com>
2018-09-12 16:01:12 +0800

    test

--- src/main/java/com/image/upload/UploadApplication.java
+++ src/main/java/com/image/upload/UploadApplication.java
@@ -2,11 +2,26 @@ package com.image.upload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import javax.servlet.MultipartConfigElement;
+@Configuration
 @SpringBootApplication
-public class UploadApplication {
+public class UploadApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(UploadApplication.class, args);
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //单个文件最大
+        factory.setMaxFileSize("10240KB"); //KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("102400KB");
+        return factory.createMultipartConfig();
     }
 }
